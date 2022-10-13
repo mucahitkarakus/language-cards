6dd6b315e25619ba303789f837b65267e5ace82e
mucahitkarakus <mocococo.80@gmail.com>
2022-10-13 14:17:21 +0300

    cart done

--- src/componenets/Cards.js
+++ src/componenets/Cards.js
@@ -3,21 +3,29 @@ import "../App.css";
 
 const Cards = ({name, img, options}) => {
   
-  const [cart , setCart] = useState(true)
+  const [cart, setCart] = useState(true);
 
   return (
-    <div className='cards' onClick={() => {
-      cart ? (
-        <div>
-          <img src={img} alt="sa" />
-          <h2>{name}</h2>
-        </div>
-      ) : (
-        <ul>
-          
-        </ul>
-      )
-    }} ></div>
+    <div className='cards' onClick={() => setCart(!cart)} >
+      {
+        cart ? (
+          <div className='sas'>
+            <img src={img} alt={name} />
+            <h2>{name}</h2>
+          </div> ) : (
+            <p>
+              {options.map((e) => {
+                return(
+                  <ul>
+                    <li>{e}</li>
+                  </ul>
+                )
+              })}
+            </p>
+          )
+        
+      }
+    </div>
   )
 }
 
